00db3018b8f9dadf3b693af1960e7260f18dc79f
tkm-mkzk <tkm.mkzk.0604@gmail.com>
2023-04-20 10:02:40 +0900

    tailwindの導入

---
+++ src/@types/global.d.ts
@@ -0,0 +1,12 @@
+/**
+ * @description API仕様
+ * ホットペッパー | APIリファレンス | リクルートWEBサービス
+ * https://webservice.recruit.co.jp/doc/hotpepper/reference.html
+ */
+
+interface ShopObj {
+  id: string
+  name: string
+  station_name: string
+  genre
+}
